f1a5a33db2965fbdd5425513eb3221000fd550dd
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-07-07 00:51:16 +0200

    install and initialize mongoose

--- server.js
+++ server.js
@@ -3,7 +3,11 @@
 let express = require('express'),
 config = require('./config/config'),
 app = express(),
+mongoose = require('mongoose'),
+db,
 server;
+
+db = mongoose.connect(config.mongo);
 
 server = app.listen(config.port);
 console.log('Server is listening on port: ', config.port);
