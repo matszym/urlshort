4be9d252a0d3e7fe70c6557dbcd9e96f793e7887
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-07-19 00:41:10 +0200

    url controllers

--- controllers/url.server.controller.js
+++ controllers/url.server.controller.js
@@ -4,9 +4,12 @@ mongoose = require('mongoose'),
 UrlModel = mongoose.model('Url');
 
 exports.validateUrl = function(req, res, next) {
-  if (req.body.originalUrl) {
-    req.url = {
-      original_url: req.body.originalUrl
+  let urlValidator = /.+:\/\/.+\..+/; // validates url
+  const userProvidedUrl = req.url.slice(5); // req.url starts with /new/
+  
+  if (userProvidedUrl.match(urlValidator)) {
+    req.myUrl = {
+      original_url: userProvidedUrl
     }
     next();
   }
@@ -18,18 +21,26 @@ exports.validateUrl = function(req, res, next) {
 exports.generateShortUrl = function(req, res, next) {
   let id = shortid.generate();
 
-  req.url._id = id;
-  req.url.short_url = req.protocol + 
-  "//" + 
-  req.host + 
+  req.myUrl._id = id;
+  req.myUrl.short_url = req.protocol + 
+  "://" + 
+  req.hostname + 
   '/' + 
   id;
-
   next();
 }
 
-exports.saveUrls = function(req, res, next) {
-  UrlModel.save(req.url)
-  .then(url => res.send(url))
-  .catch(() => exports.generateShortUrl(req, res, next));
+exports.saveUrl = function(req, res, next) {
+  UrlModel.create(req.myUrl)
+  .then(url => res.send({
+    original_url: url.original_url,
+    short_url: url.short_url
+  }))
+  .catch(e => console.log(e));
+}
+
+exports.getUrl = function(req, res, next) {
+  UrlModel.findById(req.params.id)
+  .then(doc => res.redirect(doc.original_url))
+  .catch(e => console.log(e));
 }
