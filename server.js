72bde09e5adbb1571d820c61cdcb1f9a3488501b
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-07-19 00:41:53 +0200

    server wiring (routes and db)

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ let express = require('express'),
 config = require('./config/config'),
 app = express(),
 mongoose = require('mongoose'),
+Promise = require('bluebird'),
 db,
 server;
 
+mongoose.Promise = Promise;
+
+require('./models/url.server.model.js');
+
 db = mongoose.connect(config.mongo);
+
+require('./routes/url.server.route.js')(app);
 
 server = app.listen(config.port);
 console.log('Server is listening on port: ', config.port);
